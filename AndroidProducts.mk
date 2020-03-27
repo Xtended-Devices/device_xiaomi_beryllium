5695480a5d28cd934163a6733d7d7eb49219b55f
Dev <mashru.dev@gmail.com>
2020-03-27 17:18:53 +0530

    AIMify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
+    $(LOCAL_DIR)/aim_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beryllium-user \
-    lineage_beryllium-userdebug \
-    lineage_beryllium-eng
+    aim_beryllium-user \
+    aim_beryllium-userdebug \
+    aim_beryllium-eng
