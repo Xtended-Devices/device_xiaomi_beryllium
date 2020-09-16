1552df394cec68cfd545af25d90345d0b227e057
Dev <devvmashru@protonmail.ch>
2020-09-16 18:14:18 +0530

    beryllium: clone pe display hals
    Signed-off-by: Dev <devvmashru@protonmail.ch>

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+rm -rf hardware/qcom-caf/sm8150/display
+rm -rf hardware/qcom-caf/sdm845/display
+git clone https://github.com/PixelExperience/hardware_qcom-caf_sdm845_display.git hardware/qcom-caf/sdm845/display
